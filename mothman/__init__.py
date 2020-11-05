d8b1471784dec16207941795f3c10b0910bb041a
Ong Yong Xin <ongyongxin2020@gmail.com>
2020-11-05 12:29:40 +0800

    nicer log formatting

--- mothman/__init__.py
+++ mothman/__init__.py
@@ -8,6 +8,6 @@ import coloredlogs
 from .__version__ import __version__  # noqa: F401
 
 coloredlogs.install(
-    fmt=" %(name)s :: %(levelname)-8s :: %(message)s",
+    fmt=" %(levelname)-8s :: %(message)s",
     logger=logging.getLogger("mothman"),
 )
